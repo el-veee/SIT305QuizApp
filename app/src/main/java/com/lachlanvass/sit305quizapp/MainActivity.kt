932d465ffce3433b1f60833a852ef22442791cbe
lachlan-vass <lachlan.vass@superretailgroup.com>
2022-03-20 12:04:13 +1000

    add handler logic

--- app/src/main/java/com/lachlanvass/sit305quizapp/MainActivity.kt
+++ app/src/main/java/com/lachlanvass/sit305quizapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.lachlanvass.sit305quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var appData:AppData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.start_button)
+
+
+        startButton.setOnClickListener {
+
+            val userName = findViewById<EditText>(R.id.name_input)
+
+            val userNameText = userName.text.toString()
+
+            this.appData = AppData(
+                userName = userNameText,
+                totalScore = 0,
+                questionNumber = 0
+            )
+
+            val questionOneIntent = Intent(this, QuizQuestionOne::class.java)
+
+            startActivity(questionOneIntent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/lachlanvass/sit305quizapp/QuizQuestionOne.kt
@@ -0,0 +1,11 @@
+package com.lachlanvass.sit305quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizQuestionOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_question_one)
+    }
+}
