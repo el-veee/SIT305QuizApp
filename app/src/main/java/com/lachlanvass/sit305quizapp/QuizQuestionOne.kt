2583ba76940be3415acbac82db178a6a85e2d900
lachlan-vass <lachlan.vass@superretailgroup.com>
2022-03-20 18:27:55 +1000

    set question_1 text with strings

--- app/src/main/java/com/lachlanvass/sit305quizapp/QuizQuestionOne.kt
+++ app/src/main/java/com/lachlanvass/sit305quizapp/QuizQuestionOne.kt
@@ -3,6 +3,7 @@ package com.lachlanvass.sit305quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.ToggleButton
 
 class QuizQuestionOne : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,16 +11,20 @@ class QuizQuestionOne : AppCompatActivity() {
         setContentView(R.layout.activity_quiz_question_one)
 
         val questionOne = Question(
-            "What Operating System are you using?",
-            "There are many handheld operating systems in the world",
-            "IOS",
-            "Windows",
-            "Android"
 
+            getString(R.string.question1_title),
+            getString(R.string.question1_description),
+            getString(R.string.question1_wrong_answer_1),
+            getString(R.string.question1_wrong_answer_2),
+            getString(R.string.question1_right_answer)
         )
 
         val questionTitle = findViewById<TextView>(R.id.question_title)
         val questionDetails = findViewById<TextView>(R.id.question_details)
+
+        val answerButton1 = findViewById<ToggleButton>(R.id.question_1_wrong_answer_button_1)
+
+        answerButton1.text = questionOne.wrongAnswer1
 
         questionTitle.text = questionOne.questionTitle
         questionDetails.text = questionOne.questionDetails
@@ -27,3 +32,4 @@ class QuizQuestionOne : AppCompatActivity() {
 
     }
 }
+
